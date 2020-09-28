0af36e0915aee313600f82e7dd83470b90b3f8f3
pherapont <pherapont@yandex.ru>
2020-09-28 13:50:38 +0700

    Стилизация строчки задач

--- src/componets/Tasks/index.jsx
+++ src/componets/Tasks/index.jsx
@@ -9,7 +9,18 @@ export default function Tasks() {
 			<h2 className="tasks__title">
 				Фронтенд
 				<img src={editIcon} alt="Иконка редактировать" />
-				</h2>
+			</h2>
+			<div className="tasks__item-row">
+				<div className="checkbox">
+					<input id="check" type="checkbox"/>
+					<label htmlFor="check">
+						<svg width="11" height="8" viewBox="0 0 11 8" fill="none"   xmlns="http://www.w3.org/2000/svg">
+							<path d="M9.29 1.20 L3.79 6.70 L1.29 4.20" stroke="#b2b2b2" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+						</svg>
+					</label>
+				</div>
+				<input type="text" value="ReactJS Hooks (useState, useReducer, useEffect и т.д.)" />
+			</div>
 		</div>
 )
 }
